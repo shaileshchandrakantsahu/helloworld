f20ada4319ae9ea355ac9bd710e61ac670c44ac8
shailesh.sahu@cumminscollege.edu.in <shailesh.sahu@cumminscollege.edu.in>
2022-02-28 22:47:34 +0530

    Updates

--- src/components/Greeting.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-class Greeting extends Component{
-    render(){
-        return <h1> Greeting... {this.props.name} </h1>
-    }
-}
-export default Greeting;
--- src/components/Welcome.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-class Welcome extends Component{
-    render(){
-        return <h1>Welcome Shailesh</h1>
-    }
-}
-
-export default Welcome;
